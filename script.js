5a06d14604c8eea4f23b4b368bba8fa6490dadaf
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-02-18 13:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
+    event.preventDefault(); // Prevent form submission
+    
+    let firstName = document.forms["form1"]["fname"].value.trim();
+    let lastName = document.forms["form1"]["lname"].value.trim();
+
+    alert(`${firstName} ${lastName}`);
+}
     //Write your code here
 
 }
